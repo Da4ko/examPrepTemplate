c793b5937a4b94b6d015142af6b53b382539f3dc
Da4ko <da4ko06@gmail.com>
2023-09-04 17:50:00 +0300

    Database setup and importing resources ready

---
+++ src/main/java/com/example/webexamprep/service/Impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.webexamprep.service.Impl;
+
+import com.example.webexamprep.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
---
+++ src/main/java/com/example/webexamprep/service/UserService.java
@@ -0,0 +1,4 @@
+package com.example.webexamprep.service;
+
+public interface UserService {
+}
