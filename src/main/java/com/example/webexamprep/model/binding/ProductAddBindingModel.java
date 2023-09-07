ffa7873fc66704ccb1cc9e5751b15ccb1c1c1e70
Da4ko <da4ko06@gmail.com>
2023-09-07 13:40:33 +0300

    Working on product add

--- src/main/java/com/example/webexamprep/model/binding/ProductAddBindingModel.java
+++ src/main/java/com/example/webexamprep/model/binding/ProductAddBindingModel.java
@@ -1,4 +1,63 @@
 package com.example.webexamprep.model.binding;
 
+import com.example.webexamprep.model.entity.enums.CategoryName;
+import jakarta.validation.constraints.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class ProductAddBindingModel{
-        }
+
+    private String name;
+    private String description;
+    private LocalDateTime neededBefore;
+    private BigDecimal price;
+    private CategoryName category;
+
+    public ProductAddBindingModel() {
+    }
+    @NotBlank(message = "Cannot be empty")
+    @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Size(min = 5, message = "Description must be at least 5 characters")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @FutureOrPresent(message = "The date cannot be in the past")
+    public LocalDateTime getNeededBefore() {
+        return neededBefore;
+    }
+
+    public void setNeededBefore(LocalDateTime neededBefore) {
+        this.neededBefore = neededBefore;
+    }
+
+    @DecimalMin(value = "0", message = "Price must be positive number")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+    @NotNull(message = "You must select category")
+    public CategoryName getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryName category) {
+        this.category = category;
+    }
+}
---
+++ src/main/java/com/example/webexamprep/service/Impl/ProductServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.webexamprep.service.Impl;
+
+import com.example.webexamprep.service.ProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+}
